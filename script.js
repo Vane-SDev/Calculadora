6734779d5492b6f86501891f2d73316f995c0e77
Vanesa <vanesa.soria.p@gmail.com>
2023-10-23 01:03:50 -0300

    Correccion de errores en raices

--- script.js
+++ script.js
@@ -1,113 +1,105 @@
-console.log("hello world");
 
-function agregar(valor){
-    document.getElementById('pantalla').value += valor
-}
+// // Realizar una operación matemática
+// const resultado = math.evaluate('8^2*(5-7)');
 
-function borrar(){
-    document.getElementById('pantalla').value = ''
-}
-
-function calcular(){
-    const valorPantalla = document.getElementById('pantalla').value;
-    let resultado;
-
-    //Verificar si es raiz cuadrada
-
-if (valorPantalla.includes('√')){
-    const numero =parseFloat(valorPantalla.slice(1));
-    resultado = Math.sqrt(numero);
-}
-
-//verificar si se trata de una potencia
-
-else if(valorPantalla.includes('²')){
-    const numero = parseFloat(valorPantalla);
-    resultado = Math.pow(numero, 2);
-}
-// verificar si es porcentaje
-
-else if (valorPantalla.includes('%')) {
-    const partes = valorPantalla.split('*');
-    if (partes.length === 2) {
-        const numero = parseFloat(partes[0]);
-        const porcentaje = parseFloat(partes[1].replace('%', '')) / 100;
-        if (!Number.isNaN(numero) && !Number.isNaN(porcentaje)) {
-            resultado = numero * porcentaje;
+// // Mostrar el resultado en la consola
+// console.log('Resultado:', resultado);
+function agregar(valor) {
+    const pantalla = document.getElementById('pantalla');
+    const indice = document.getElementById('indice').value;
+    
+    if (indice) {
+        if (valor === '√') {
+            pantalla.value += `sqrt(${indice}, `;
+        } else {
+            pantalla.value += valor;
         }
+        document.getElementById('indice').value = ''; // Limpia el campo de índice
+    } else {
+        pantalla.value += valor;
     }
 }
-// para el resto de las operaciones
 
-else{
-    console.log(valorPantalla)
-    resultado = eval(valorPantalla)
+
+function borrar() {
+    document.getElementById('pantalla').value = '';
+}
+function calcular() {
+    let valorPantalla = document.getElementById('pantalla').value;
+    let resultado;
+
+    try {
+        // Reemplazar "^" con "**" para las potencias
+        valorPantalla = valorPantalla.replace(/\^/g, '**');
+        // Reemplazar el símbolo de porcentaje (%) con "/100"
+        valorPantalla = valorPantalla.replace(/%/g, '/100');
+        
+        // Buscar y calcular todas las raíces con cualquier índice
+        const regex = /(\d+(?:\.\d+)?)√(\d+(?:\.\d+)?)/g;
+        valorPantalla = valorPantalla.replace(regex, function (match, indice, radicando) {
+            return Math.pow(parseFloat(radicando), 1 / parseFloat(indice));
+        });
+        valorPantalla = valorPantalla.replace(/\*\*/g, '^');
+        // Calcular la expresión con Math.js
+        resultado = math.evaluate(valorPantalla);
+    } catch (error) {
+        console.error("Error en la expresión matemática:", error);
+        resultado = "Error";
+    }
+
+    document.getElementById('pantalla').value = resultado;
 }
 
-    document.getElementById('pantalla').value = resultado
-}
 
-function retroceder (){
+
+
+function retroceder() {
     const pantalla = document.getElementById('pantalla');
     pantalla.value = pantalla.value.slice(0, -1);
 }
 
-function agregarParentesis(valor){
+function agregarParentesis(valor) {
     const pantalla = document.getElementById('pantalla');
     pantalla.value += valor;
 }
 
-function quitarParentesis (){
+function quitarParentesis() {
     const pantalla = document.getElementById('pantalla');
     const valorActual = pantalla.value;
 
-    if(valorActual.endsWith(')')){
-        // si el caracter es un parentesis de cierre, se elimina
+    if (valorActual.endsWith(')')) {
         pantalla.value = valorActual.slice(0, -1);
-}else{
-    //si no hay parentesis de cierre,  agrego uno
-    pantalla.value += ')';
+    } else {
+        pantalla.value += ')';
     }
 }
 
-function cambiarSigno(){
+function cambiarSigno() {
     const pantalla = document.getElementById('pantalla');
     let valorActual = pantalla.value;
 
-    //verificamos si el valor de la pantalla es un numero
-
-    if(valorActual.startsWith('-')){
-
-        //si el numero ya es negativo, se cambia a positivo
+    if (valorActual.startsWith('-')) {
         valorActual = valorActual.substring(1);
-
-        // si es un numero 
-    }else{
+    } else {
         valorActual = '-' + valorActual;
     }
 
     pantalla.value = valorActual;
 }
 
-    document.addEventListener('keydown', function(event){
+document.addEventListener('keydown', function (event) {
+    const tecla = event.key;
+    const pantalla = document.getElementById('pantalla');
 
-        const tecla = event.key;
-        const pantalla = document.getElementById('pantalla');
-
-        // verificamos si la tecla que presiona es guion medio
-        if (tecla === '-' || (event.shiftKey && tecla === '-')){
-            //verificamos si el guion medio se tiene que interpretar como signo negativo
-            if(pantalla.value === ''){
-                // si la pantalla está vacía, se añade el signo negativo al principio
-                pantalla.value = '-';
-            }else{
-                //si la pantalla no está vacía, se agrega el guion medio como operador
-                agregar(tecla);
-            }
-            event.preventDefault(); // con esto evitamos que el guion medio se muestre en el campo de entrada
-        }else{
-            // con esto manejamos las otras teclas
-        switch (tecla){
+    if (tecla === '-' || (event.shiftKey && tecla === '-')) {
+        if (pantalla.value === '') {
+            pantalla.value = '-';
+        } else {
+            agregar(tecla);
+        }
+        event.preventDefault();
+    } else {
+        switch (tecla) {
             case '0':
             case '1':
             case '2':
@@ -118,15 +110,13 @@ function cambiarSigno(){
             case '7':
             case '8':
             case '9':
-                agregar(tecla);
-                break;
-            case'+':
-            case'-':
-            case'*':
-            case'/':
-            case'%':
-            case'(':
-            case')':
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '%':
+            case '(':
+            case ')':
                 agregar(tecla);
                 break;
             case 'Enter':
@@ -139,7 +129,6 @@ function cambiarSigno(){
                 cambiarSigno();
                 break;
             default:
-                //no se hace nada si no se presiona una tecla no manejada
         }
     }
 });
